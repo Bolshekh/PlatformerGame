7e2cf1da24c9ddbe08164fd4d66f67950ac4d583
nevzorov_ae <nevzorov_ae@transset.ru>
2024-12-04 16:25:41 +0700

    parallax done, textrures done
    can use some normal textures, no pixel perfect, not even same color pallet. maybe need to redraw platform texture

---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+	[SerializeField] GameObject ParticlesRef;
+	ParticleSystem particle;
+	private void Start()
+	{
+		PlayerMovement pm = GetComponent<PlayerMovement>();
+		particle = ParticlesRef.GetComponent<ParticleSystem>();
+
+		pm.GroundCheckChanged += (s, e) =>
+		{
+			var em = particle.emission;
+			em.rateOverDistance = e.CustomVariable ? 1 : 0;
+		};
+	}
+}
